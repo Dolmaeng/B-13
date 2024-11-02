201337383b36e1053f4f8a1a54eaa1281f34a465
Leo <insoyafear@gmail.com>
2024-11-02 13:35:47 +0900

    smile face added

--- b-13/app/layout.tsx
+++ b-13/app/layout.tsx
@@ -1,11 +1,12 @@
 // app/layout.tsx
+import { Analytics } from "@vercel/analytics/react";
 import './globals.css';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-    title: 'Composer Dating Sim',
-    description: 'A composer dating simulation game',
+    title: '작곡가 연애 시뮬레이션!',
+    description: '유명 작곡가들과 두근두근 데이트!!',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                     content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
                 />
             </head>
+                <Analytics />
             <body>{children}</body>
         </html>
     );
