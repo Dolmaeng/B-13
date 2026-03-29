99fa2965fea230d66565e255481fab63bd00a4ba
dolmaeng <insoyafear@gmail.com>
2026-03-29 17:58:44 +0900

    hotfix

--- b-13/app/layout.tsx
+++ b-13/app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                     content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
                 />
             </head>
+            <body>
+                {children}
                 <Analytics />
-            <body>{children}</body>
+            </body>
         </html>
     );
 }
